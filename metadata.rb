006eae4cd581249fb9c9c93c17ed17dc26acb014
Stefhen Hovland <stefhen@rightscale.com>
2015-02-23 14:44:35 -0500

    Simple metadata update

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ description      'Installs/Configures rsc_ros'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+%w{ ubuntu }.each do |os|
+  supports os
+end
