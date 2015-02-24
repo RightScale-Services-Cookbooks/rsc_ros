cc65d7fbca3c6053c9484babef9e6e286a120f9d
Stefhen Hovland <stefhen@rightscale.com>
2015-02-24 12:45:33 -0500

    Add inputs

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'rsc_ros'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'RightScale'
+maintainer_email 'ps@rightscale.com'
 license          'All rights reserved'
 description      'Installs/Configures rsc_ros'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -9,3 +9,25 @@ version          '0.1.0'
 %w{ ubuntu }.each do |os|
   supports os
 end
+
+attribute 'rsc_ros/storage_provider',
+  :display_name => 'Storage Provider',
+  :description  => 'Storage provider which houses file to download.',
+  :type         => 'string',
+  :recipes      => ['rsc_ros::configure_fog'],
+  :choice       => ['aws', 'rackspace', 'google'],
+  :required     => 'required'
+
+attribute 'rsc_ros/access_key',
+  :display_name => 'Access Key',
+  :description  => 'Access key to use with storage provider.',
+  :type         => 'string',
+  :recipes      => ['rsc_ros::configure_fog'],
+  :required     => 'required'
+
+attribute 'rsc_ros/secret_key',
+  :display_name => 'Secret Key',
+  :description  => 'Secret key to use with storage provider.',
+  :type         => 'string',
+  :recipes      => ['rsc_ros::configure_fog'],
+  :required     => 'required'
