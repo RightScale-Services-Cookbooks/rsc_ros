141ae4f81ed8b35360235587cc9165a9de427c2e
Richard Shade <rshade@rightscale.com>
2015-03-02 15:00:55 -0600

    moving case to recipe

--- recipes/download.rb
+++ recipes/download.rb
@@ -9,15 +9,33 @@
 
 download_script = "/tmp/rsc_ros_#{SecureRandom.urlsafe_base64}.rb"
 
+@credentials={}
+@credentials[:provider]=node[:rsc_ros][:storage_provider]
+case node[:rsc_ros][:storage_provider].downcase
+when "aws"
+  @credentials={
+    :aws_access_key_id => node[:rsc_ros][:access_key],
+    :aws_secret_access_key => node[:rsc_ros][:secret_key]
+  }
+when "rackspace"
+  @credentials={
+    :rackspace_username => node[:rsc_ros][:access_key],
+    :rackspace_api_key  => node[:rsc_ros][:secret_key]
+  }
+when "google"
+  @credentials={
+    :google_storage_access_key_id => node[:rsc_ros][:access_key],
+    :google_storage_secret_access_key => node[:rsc_ros][:secret_key]
+  }
+end
+
 template download_script do
   source "download.erb"
   owner "root"
   group "root"
   mode 0700
   variables({
-    :provider    => node[:rsc_ros][:storage_provider],
-    :access_key  => node[:rsc_ros][:access_key],
-    :secret_key  => node[:rsc_ros][:secret_key],
+    :credentials => @credentials,
     :bucket      => node[:rsc_ros][:bucket],
     :file        => node[:rsc_ros][:file],
     :destination => node[:rsc_ros][:destination]
