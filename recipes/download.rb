b79ecec4afd527d3e24585e89e8a68f3ae8df37d
Richard Shade <rshade@rightscale.com>
2015-03-04 16:33:09 -0600

    adding include_recipe and hard coded destination

--- recipes/download.rb
+++ recipes/download.rb
@@ -1,8 +1,11 @@
+include_recipe "rsc_ros::default"
+
 rsc_ros node[:rsc_ros][:destination] do
   provider node[:rsc_ros][:provider]
   access_key node[:rsc_ros][:access_key]
   secret_key node[:rsc_ros][:secret_key]
   bucket node[:rsc_ros][:bucket]
   file node[:rsc_ros][:file]
+  destination node[:rsc_ros][:destination]
   action :download
 end
