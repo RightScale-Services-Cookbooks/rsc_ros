32d570fe13c5809cd2bc108e403d1cb05f5dd54f
Stefhen Hovland <stefhen@rightscale.com>
2015-03-09 15:49:05 -0400

    Only install fog to system wide ruby installation.

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,25 +13,10 @@ node[:rsc_ros][:packages].each do |pkg|
   p.run_action(:install)
 end
 
-#INSTALL GEMS
-gem 'mixlib-cli' do
+gem_package 'fog' do
   action :install
   gem_binary "/usr/bin/gem"
   options("--no-rdoc --no-ri")
-end
-
-gem 'fog' do
-  action :install
-  gem_binary "/usr/bin/gem"
-  options("--no-rdoc --no-ri")
-end
-
-chef_gem 'mixlib-cli' do
-  action :install
-end
-
-chef_gem 'fog' do
-  action :install
 end
 
 cookbook_file "/usr/local/bin/ros_download.rb" do
