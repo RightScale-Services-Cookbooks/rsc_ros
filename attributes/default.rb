50c3d0e47ff73ba65dbb6a614340e49c3f828ad1
Stefhen Hovland <stefhen@rightscale.com>
2015-04-22 18:16:36 -0400

    Add make to package install list

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ when "ubuntu", "debian"
   # Base ServerTemplate for Linux (v14.1.0) [rev 50]
   # Ubuntu 14.04
   #default[:rsc_ros][:packages] = %w[ruby ruby-dev patch autoconf automake gcc g++]
-  default[:rsc_ros][:packages] = %w[ruby ruby-dev autoconf automake g++ patch]
+  default[:rsc_ros][:packages] = %w[ruby ruby-dev make autoconf automake g++ patch]
   default[:rsc_ros][:gems]     = %w[fog]
 
 when "redhat", "centos"
