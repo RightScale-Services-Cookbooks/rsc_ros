15d2356db4bc43664e72eeed4be73424dd987313
Stefhen Hovland <stefhen@rightscale.com>
2015-04-23 17:03:20 -0400

    Add centos support

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures rsc_ros'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w{ ubuntu }.each do |os|
+%w{ centos ubuntu }.each do |os|
   supports os
 end
 
