3ec68b94b65f9df519a0441d780c5a392bb735b2
Stefhen Hovland <stefhen@rightscale.com>
2015-04-23 12:32:54 -0400

    CentOS and attribute comments

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
 case node[:platform]
 when "ubuntu", "debian"
-  # Base ServerTemplate for Linux (v14.1.0) [rev 50]
-  # Ubuntu 14.04
-  #default[:rsc_ros][:packages] = %w[ruby ruby-dev patch autoconf automake gcc g++]
+  # Tested with: Base ServerTemplate for Linux (v14.1.0) [rev 50] / Ubuntu 14.04
   default[:rsc_ros][:packages] = %w[ruby ruby-dev make autoconf automake g++ patch]
   default[:rsc_ros][:gems]     = %w[mixlib-cli fog]
 
 when "redhat", "centos"
+  # Tested with: Base ServerTemplate for Linux (v14.1.0) [rev 50] / CentOS 6.6
   default[:rsc_ros][:packages] = %w[ruby ruby-devel rubygems]
+  default[:rsc_ros][:gems]     = %w[mixlib-cli fog]
 end
