c4cc881fdc0bd900708ab7d39190055b61d1cb7a
Stefhen Hovland <stefhen@rightscale.com>
2015-04-23 17:01:47 -0400

    Install versioned gems for ubuntu

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ case node[:platform]
 when "ubuntu", "debian"
   # Tested with: Base ServerTemplate for Linux (v14.1.0) [rev 50] / Ubuntu 14.04
   default[:rsc_ros][:packages] = %w[ruby ruby-dev make autoconf automake g++ patch]
-  default[:rsc_ros][:gems]     = %w[mixlib-cli fog]
+  default[:rsc_ros][:gems]     = { "fog" => "1.24.0", "mixlib-cli" => "1.5.0" }
 
 when "redhat", "centos"
   # Tested with: Base ServerTemplate for Linux (v14.1.0) [rev 50] / CentOS 6.5
