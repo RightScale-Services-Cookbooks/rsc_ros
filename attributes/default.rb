f6edda0c23065b397a0e614ab179de98c7144ddc
Stefhen Hovland <stefhen@rightscale.com>
2015-04-23 21:03:10 -0400

    Use fog 1.29.0, GCE download broken on 1.24.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,10 +2,10 @@ case node[:platform]
 when "ubuntu", "debian"
   # Tested with: Base ServerTemplate for Linux (v14.1.0) [rev 50] / Ubuntu 14.04
   default[:rsc_ros][:packages] = %w[ruby ruby-dev make autoconf automake g++ patch]
-  default[:rsc_ros][:gems]     = { "fog" => "1.24.0", "mixlib-cli" => "1.5.0" }
+  default[:rsc_ros][:gems]     = { "fog" => "1.29.0", "mixlib-cli" => "1.5.0" }
 
 when "redhat", "centos"
   # Tested with: Base ServerTemplate for Linux (v14.1.0) [rev 50] / CentOS 6.5
   default[:rsc_ros][:packages] = %w[ruby19 ruby19-devel gcc-c++]
-  default[:rsc_ros][:gems]     = { "fog" => "1.24.0", "mixlib-cli" => "1.5.0" }
+  default[:rsc_ros][:gems]     = { "fog" => "1.29.0", "mixlib-cli" => "1.5.0" }
 end
