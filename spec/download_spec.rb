53b806bd46a5680f0c1264ba17e6fc47584ee59f
Richard Shade <rshade@rightscale.com>
2015-04-24 17:30:35 -0500

    adding coverage stuff there

--- spec/download_spec.rb
+++ spec/download_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe 'rsc_ros::download' do
   let(:chef_run) do
-    ChefSpec::Runner.new do |node|
+    ChefSpec::Runner.new(platform: 'centos', version: '6.5') do |node|
       node.set['rsc_ros']['destination'] = '/tmp/filename'
     end.converge(described_recipe)
   end
