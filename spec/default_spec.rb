30a848030b4deae95ec578e3b49d29fa6a7692e0
Stefhen Hovland <stefhen@rightscale.com>
2015-04-24 16:19:24 -0400

    Add simple ruby package check

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,6 +3,10 @@ require_relative 'spec_helper'
 describe 'rsc_ros::default' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
+  it 'installs ruby' do
+    expect(chef_run).to install_package('ruby')
+  end
+
   it 'creates /usr/local/bin/ros_download.rb' do
     expect(chef_run).to create_cookbook_file("/usr/local/bin/ros_download.rb")
   end
