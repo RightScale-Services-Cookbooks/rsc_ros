f8438ff869500b183bd88305ca95f72c2845bbf4
Richard Shade <rshade@rightscale.com>
2015-04-24 15:28:58 -0500

    adding node to recipe

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,7 +1,11 @@
 require_relative 'spec_helper'
 
 describe 'rsc_ros::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) do
+    ChefSpec::Runner.new do |node|
+      node.set['rsc_ros']['packages']=%w[ruby ruby-dev make autoconf automake g++ patch zlib1g-dev]
+    end.converge(described_recipe)
+  end
 
   it 'installs ruby' do
     expect(chef_run).to install_package('ruby')
