3e2e1a9f3c9751e95be53a4a2b4fb2a256880a8c
Stefhen Hovland <stefhen@rightscale.com>
2015-04-29 13:13:09 -0400

    Add packages

--- test/cookbooks/fake/recipes/install_rightscale_epel.rb
+++ test/cookbooks/fake/recipes/install_rightscale_epel.rb
@@ -5,6 +5,10 @@ cookbook_file '/etc/pki/rpm-gpg/RPM-GPG-KEY-RightScale' do
   mode 0644
 end
 
+execute 'import rightscale gpg key' do
+  command 'rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-RightScale'
+end
+
 cookbook_file '/etc/yum.repos.d/rightscale-epel.repo' do
   source 'rightscale-epel.repo'
   owner 'root'
