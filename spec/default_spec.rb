52af8da0a51709a85d75efe44be0de70dd7e19c2
Stefhen Hovland <stefhen@rightscale.com>
2015-04-29 13:15:21 -0400

    syntax

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -5,28 +5,29 @@ describe 'rsc_ros::default' do
   let(:chef_run_centos) { ChefSpec::Runner.new(platform: 'centos', version: '6.5').converge(described_recipe) }
 
   it 'installs required ubuntu packages' do
-    expect(chef_run_ubuntu).to install_package("ruby")
-    expect(chef_run_ubuntu).to install_package("ruby-dev")
-    expect(chef_run_ubuntu).to install_package("make")
-    expect(chef_run_ubuntu).to install_package("autoconf")
-    expect(chef_run_ubuntu).to install_package("automake")
-    expect(chef_run_ubuntu).to install_package("g++")
-    expect(chef_run_ubuntu).to install_package("patch")
-    expect(chef_run_ubuntu).to install_package("zlib1g-dev")
+    expect(chef_run_ubuntu).to install_package('ruby')
+    expect(chef_run_ubuntu).to install_package('ruby-dev')
+    expect(chef_run_ubuntu).to install_package('make')
+    expect(chef_run_ubuntu).to install_package('autoconf')
+    expect(chef_run_ubuntu).to install_package('automake')
+    expect(chef_run_ubuntu).to install_package('g++')
+    expect(chef_run_ubuntu).to install_package('patch')
+    expect(chef_run_ubuntu).to install_package('zlib1g-dev')
   end
 
   it 'installs required centos packages' do
-    expect(chef_run_centos).to install_package("ruby19")
-    expect(chef_run_centos).to install_package("ruby19-devel")
-    expect(chef_run_centos).to install_package("gcc-c++")
+    expect(chef_run_centos).to install_package('ruby19')
+    expect(chef_run_centos).to install_package('ruby19-devel')
+    expect(chef_run_centos).to install_package('gcc-c++')
+    expect(chef_run_centos).to install_package('zlib-devel')
   end
 
   it 'installs required gems' do
-    expect(chef_run_ubuntu).to install_gem_package("fog")
-    expect(chef_run_ubuntu).to install_gem_package("mixlib-cli")
+    expect(chef_run_ubuntu).to install_gem_package('fog')
+    expect(chef_run_ubuntu).to install_gem_package('mixlib-cli')
   end
 
   it 'creates /usr/local/bin/ros_download.rb' do
-    expect(chef_run_ubuntu).to create_cookbook_file("/usr/local/bin/ros_download.rb")
+    expect(chef_run_ubuntu).to create_cookbook_file('/usr/local/bin/ros_download.rb')
   end
 end
