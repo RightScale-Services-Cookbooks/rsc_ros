7ab6a6b99a14fcfa00e9ae27ed15a3d53e07a38c
Bryan Karaffa <bryankaraffa@gmail.com>
2016-09-15 14:30:20 -0700

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,5 +10,5 @@ else
   raise "Sorry #{node['platform']} is not supported ..."
 end
 
-default['rsc_ros']['gems'] = { "fog" => "1.36.0", "mixlib-cli" => "1.5.0", 'fog-profitblocks'=>'0.0.5' }
+default['rsc_ros']['gems'] = { "fog" => "1.36.0", "mixlib-cli" => "1.5.0", 'mime-types'=>'1.25.0', 'fog-profitblocks'=>'0.0.5' }
 default['rsc_ros']['timeout']=1200
