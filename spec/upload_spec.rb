7fe2d514fe34f4573c24b177432b42e21d101961
Richard Shade <rshade@rightscale.com>
2016-09-19 14:19:53 -0500

    updating node resources

--- spec/upload_spec.rb
+++ spec/upload_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe 'rsc_ros::upload' do
   let(:chef_run) do
     ChefSpec::Runner.new(platform: 'centos', version: '6.5') do |node|
-      node.set['rsc_ros']['file'] = '/tmp/archive_download.tar.bz2'
+      node.set['rsc_ros']['destination'] = '/tmp/archive_download.tar.bz2'
+      node.set['rsc_ros']['storage_provider'] = 'aws'
+      node.set['rsc_ros']['access_key'] = 'key'
+      node.set['rsc_ros']['secret_key'] = 'key'
+      node.set['rsc_ros']['bucket'] = 'bucket'
+      node.set['rsc_ros']['destination'] = 'destination'
+      node.set['rsc_ros']['file'] = 'file'
     end.converge(described_recipe)
   end
 
